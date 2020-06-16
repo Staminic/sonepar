d2e8b4d4a6f8960b363f347645004d794c236f00
Simon <simon.delmotte@staminic.com>
2020-06-16 15:32:23 +0200

    Sticky Footer pour la page d'accueil également

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once JPATH_THEMES.'/'.$this->template.'/inc/logic.php';
 <head>
 <jdoc:include type="head" /><?php include_once JPATH_THEMES.'/'.$this->template.'/inc/head.php'; ?>
 
-<script>
+<!-- <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -16,7 +16,7 @@ include_once JPATH_THEMES.'/'.$this->template.'/inc/logic.php';
   ga('create', 'UA-102755396-1', 'auto');
   ga('send', 'pageview');
 
-</script>
+</script> -->
 </head>
 
 <body id="origin" class="<?php
